027e7aa4de6a94b106cc94215d6c19cda63c247f
Narusha Arulranjan <narulranjan@myseneca.ca>
2025-02-17 20:51:05 -0500

    lab 4 submission

---
+++ lab4/lab4c.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python3
+
+dict_york = {'Address': '70 The Pond Rd', 'City': 'Toronto', 'Country': 'Canada', 'Postal Code': 'M3J3M6', 'Province': 'ON'}
+dict_newnham = {'Address': '1750 Finch Ave E', 'City': 'Toronto', 'Country': 'Canada', 'Postal Code': 'M2J2X5', 'Province': 'ON'}
+
+list_keys = ['Address', 'City', 'Country', 'Postal Code', 'Province']
+list_values = ['70 The Pond Rd', 'Toronto', 'Canada', 'M3J3M6', 'ON']
+
+def create_dictionary(keys, values):
+    new_dict = {}
+    index = 0
+    while index < len(keys):
+        new_dict[keys[index]] = values[index]
+        index += 1
+    return new_dict
+
+def shared_values(dict1, dict2):
+    values1 = set(dict1.values())
+    values2 = set(dict2.values())
+    return values1.intersection(values2)
+
+if __name__ == '__main__':
+    york = create_dictionary(list_keys, list_values)
+    print('York: ', york)
+    common = shared_values(dict_york, dict_newnham)
+    print('Shared Values: ', common)
