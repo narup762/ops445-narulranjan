85a518cfd4c0dfecdc976d15b21af9ba3a13b87a
Narusha Arulranjan <narulranjan@myseneca.ca>
2025-02-17 20:21:53 -0500

    lab 3 submission

---
+++ lab3/lab3a.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+
+def return_text_value():
+    return "Good Morning Terry"
+
+def return_number_value():
+    return 7 + 8
+
+if __name__ == "__main__":
+    print("python code")
+    text = return_text_value()
+    print(text)
+    number = return_number_value()
+    print(str(number))
---
+++ lab3/lab3c.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python3
+
+def operate(number1, number2, operator):
+    if operator == 'add':
+        result = number1 + number2
+    elif operator == 'subtract':
+        result = number1 - number2
+    elif operator == 'multiply':
+        result = number1 * number2
+    else:
+        result = 'Error: function operator can be "add", "subtract", or "multiply"'
+    
+    return result
+
+if __name__ == '__main__':
+    print(operate(10, 5, 'add'))
+    print(operate(10, 5, 'subtract'))
+    print(operate(10, 5, 'multiply'))
+    print(operate(10, 5, 'divide'))
